aeced8a1a560c31cb3b646b542b4eaf2acab4591
mansi <dobariyamansi121@gmail.com>
2023-12-17 10:37:35 +0530

    add teacher router

---
+++ src/layouts/teacher/Teacher.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import DataGrid from 'devextreme-react';
+import { customers } from './data.js';
+
+const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+const Teacher = () => (
+    <></>
+//   <DataGrid
+//     dataSource={customers}
+//     keyExpr="ID"
+//     defaultColumns={columns}
+//     showBorders={true}
+//   />
+);
+export default Teacher;
