f237710aeb560f5670206e9d66b5dd065c64da70
mansi <dobariyamansi121@gmail.com>
2024-03-04 18:46:22 +0530

    tasks page create api and update api add

--- src/layouts/authentication/assignments/Details.js
+++ src/layouts/authentication/assignments/Details.js
@@ -19,7 +19,7 @@ const Details = () => {
             <p >Summary</p>
             <div className="card-Details2 border border-light">
               {/* <p>Works everywhere, whether on a PC, tablet, or mobile device.</p> */}
-              <p>{location?.state?.assignmentSummary}</p>
+              <p>{location?.state?.assignmentDetail?.assignmentSummary}</p>
             </div>
           </div>
           <p className="card-assignment"> </p>
@@ -28,7 +28,7 @@ const Details = () => {
           <div style={{ paddingLeft: "22px", paddingRight: "9px", color: "#344767" }}>
             <p >Description</p>
             {/* <p style={{ fontSize: "smaller", paddingBottom: "13px", paddingTop: "13px" }}>Reports may have different structures, depending on the discipline and the purpose. Below is an example report structure, but of course you are should always check your assignment instructions to see what format or structure they suggest, and if in doubt, check with your teacher.</p> */}
-            <p style={{ fontSize: "smaller", paddingBottom: "13px", paddingTop: "13px" }}>{location?.state?.projectDescription}</p>
+            <p style={{ fontSize: "smaller", paddingBottom: "13px", paddingTop: "13px" }}>{location?.state?.assignmentDetail?.projectDescription}</p>
           </div>
 
 
